7b54390c9371ab134de2ab193f5ed21c3ac33314
mrpaint <mrpaint@ec576b19-85b6-78d5-4ca2-c9a7de68a13b>
2010-05-01 03:14:20 +0000

    Testing

--- trunk/src/TestTypeAsStructure.java
+++ trunk/src/TestTypeAsStructure.java
@@ -12,13 +12,19 @@ public class TestTypeAsStructure {
 	 */
 	public static void main(String[] args) throws StructureException {
 		// TODO Auto-generated method stub
-		new Class("A");
-		Structure test = new Property("a","LinkedList<A>");
-		Structure[] types = test.getTypeAsStructure();
+		Structure A = new Class("A");
+		Structure B = new Class("B");
+		Structure C = new Class("C");
+		Structure p1 = new Property("p1","Vector<B>");
+		
+		B.add(C);
+		
+		Structure[] types = p1.getTypeAsStructure();
 		for (int i = 0; i < types.length; i++) {
-			System.err.println(types[i]);
+			System.out.println("types[" + i + "] = " + types[i]);
+			System.out.println(types[i].checkAllowedModifiers());
 		}
-		System.err.println("FML");
+		System.out.println("END");
 	}
 
 }
