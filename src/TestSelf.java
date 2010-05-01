9a89fd21ee6b36f1fb98fd961e96a2c6dfd408b6
mrpaint <mrpaint@ec576b19-85b6-78d5-4ca2-c9a7de68a13b>
2010-05-01 03:16:20 +0000

    Mine is correct!

--- src/TestSelf.java
+++ src/TestSelf.java
@@ -12,6 +12,33 @@ import com.tranvietson.uml.structures.StructureListener;
 
 public class TestSelf implements StructureListener {
 	Diagram d;
+	
+	public TestSelf() throws StructureException {
+		//fuck you man
+		d = new Diagram();
+		JFrame f = new JFrame();
+		f.add(d);
+		f.pack();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		
+		Structure.global_listener = this;
+		
+		Structure sStructure = new Class("Structure","public");
+		sStructure.add(new Property("children","List<Structure>"));
+		sStructure.add(new Method("add","boolean"));
+		Structure sClass = new Class("Class","public"); sStructure.add(sClass);
+		Structure sInterface = new Class("Interface","public"); sStructure.add(sInterface);
+		Structure sMethod = new Class("Method","public"); sStructure.add(sMethod);
+		Structure sProperty = new Class("Property","public"); sStructure.add(sProperty);
+		
+		Structure sRelationship = new Class("Relationship","public");
+		Structure sGeneralizationRelationship = new Class("GeneralizationRelationship","public"); sRelationship.add(sGeneralizationRelationship);
+		Structure sMultiplicityRelationship = new Class("MultiplicityRelationship","public"); sRelationship.add(sMultiplicityRelationship);
+		
+		Structure sDiagram = new Class("Diagram","public");
+		sDiagram.add(new Property("structures","List<Structure>"));
+	}
 
 	@Override
 	public void structureChanged(StructureEvent e) {
